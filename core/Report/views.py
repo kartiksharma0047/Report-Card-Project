b155bd90f3cf3ec9be93de838c025c6345f5c991
kartiksharma0047 <kartiksharma55109@gmail.com>
2025-06-17 06:11:57 +0530

    Some Changes are done

--- core/Report/views.py
+++ core/Report/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from Report.models import *
+from django.db.models import Q 
+from django.core.paginator import Paginator
 # Create your views here.
+def get_students(request):
+    querySet = Student.objects.all()  
+    
+    if request.GET.get('search'):
+        search=request.GET.get('search')
+        querySet=querySet.filter(name__icontains=search)
+    
+    paginator=Paginator(querySet,6)
+    page_number=request.GET.get("page",1)
+    page_obj=paginator.get_page(page_number)
+    return render(request, './Pages/student.html', context={'Data': page_obj})
--- core/core/urls.py
+++ core/core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Report.views import *
 
 urlpatterns = [
+    path('',get_students,name="get_students"),
     path('admin/', admin.site.urls),
 ]
